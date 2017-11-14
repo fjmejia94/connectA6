77a8fc0a8167ea0233202fb4b129133feb60f708
Gabriel Ang <gang@ucsd.edu>
2017-11-13 18:49:12 -0800

    Implemented calendar event importing

--- script.js
+++ script.js
@@ -12,21 +12,6 @@ function saveEvent(){
   // Figure which event you're on if on a single day
   var eventNum = sessionStorage.getItem('eventIndex');
 
-  /*
-  // Save input local
-  var eventName = document.getElementById('eventNameInput').value;
-  var eventDate = document.getElementById('dateInput').value;
-
-  // Store data into storage sesh using accessString as hash key
-
-  accessString = toString(eventNum) + "(@)" + eventDate + "(@)" + "eventName";
-  sessionStorage.setItem(accessString, eventName);
-
-  accessString = eventNum + eventDate + "(@)" + "eventDate";
-  sessionStorage.setItem(accessString, eventDate);
-
-  */
-
   /* Using JSON.stringify */
   var eventObj =
   {
@@ -34,10 +19,9 @@ function saveEvent(){
     date: document.getElementById('dateInput').value,
     descrip: document.getElementById('descripInput').value
   };
-  console.log(eventNum);
   var eventStr = 'event' + eventNum;
+  console.log(eventStr);
   sessionStorage.setItem(eventStr, JSON.stringify(eventObj));
-  displayEvent();
 
   // Increment event number
   eventNum = parseInt(eventNum) + 1;
@@ -50,37 +34,29 @@ function saveEvent(){
 
 }
 
-
-function incTest(){
-  document.getElementById('eventNameInput').value = "";
-  document.getElementById('dateInput').value = "0001-01-01";
-
-  dayArray.push(1);
-  console.log(document.getElementById('dateInput').value);
-}
-
-
-function displayEvent(){
-  var desiredDate = "2017-11-07";
+function displayEvent( day ){
+  var desiredDate = "2017-11-" + day;
   var eventNum = parseInt(sessionStorage.getItem('eventIndex'));
-  console.log(eventNum);
-  for (i=0;i<eventNum+1;i++){
+  console.log(desiredDate);
+  for (i=0;i<eventNum;i++){
     var eventStr = 'event' + i;
     var eventObj = JSON.parse(sessionStorage.getItem(eventStr));
-
     if (eventObj.date == desiredDate){
-      console.log(i);
       console.log(eventObj);
-      document.getElementById('some_message').innerHTML = eventObj.name;
-      document.getElementById('some_message2').innerHTML = eventObj.date;
-      document.getElementById('some_message3').innerHTML = eventObj.descrip;
+      document.getElementById('dayContent1').innerHTML = eventObj.name;
+      document.getElementById('dayContent2').innerHTML = eventObj.date;
+      document.getElementById('dayContent3').innerHTML = eventObj.descrip;
 
     }
   }
 }
 
 function selectDay( day ){
+  var dayStr = "day" + parseInt(selectedDay);
+  document.getElementById(dayStr).className -= "active";
+  dayStr = "day" + parseInt(day);
+  document.getElementById(dayStr).className = "active";
   selectedDay = day;
-  console.log(selectedDay);
-  document.getElementById('day1').className = "active";
+
+  displayEvent(selectedDay);
 }
